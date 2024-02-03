52aa1ef913edfd90bef3a9a5ffa63bdb150382d1
PHNakamura <paulo_hn@hotmail.com>
2024-02-03 09:41:45 -0400

    Atulaização 01

---
+++ ex02.php
@@ -0,0 +1,11 @@
+<?php
+
+// Atividade proposta pelo Matheus Batisti (Hora de codar).
+// Exercício 02 - Verificação de acesso.
+// 1- Escreva uma função chamada [verificarAcesso] que receba dois parâmetros: um número inteiro representando a idade de uma pessoa e um booleano indicando se a pessoa possui autorização de acesso.
+// 2- A função deve retornar uma string indicando se a pessoa pode ou não ter acesso a um determinado local;
+// 3- Considere as seguintes condições:
+//      - Se a idade for maior ou igual a 18 anos e autorização for verdadeira, retorne a string "Acesso autorizado";
+//      - Se a idade for menor que 18 anos, retorne a string "Acesso negado. Idade mínima requerida: 18 anos";
+//      - Se a idade for maior ou igual a 18 anos, mas a autorização for falsa, retorne a string "Acesso negado. Autorização necessária".
+
