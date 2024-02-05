d197027c1cab737d36a73956334459b63d0df0bb
PHNakamura <paulo_hn@hotmail.com>
2024-02-05 15:27:27 -0400

    Enunciado do exercicio

---
+++ ex03.php
@@ -0,0 +1,17 @@
+<?php
+
+    // Atividade proposta pelo Matheus Batisti (Hora de codar).
+    // Exercício 03 - Cálculo de desconto.
+    // 1- Escrever uma função chamada [calcularDesconto] que receba dois parametros: o valor de um produto e uma categoria.
+    // 2- A função deve retornar o valor do produto com o desconto aplicado, de acordo com a categoria.
+    // 3- Considere as seguintes categorias e seus respectivos descontos:
+    //      - Eletrônico: 10% de desconto;
+    //      - Vestuário: 20% de desconto;
+    //      - Alimentos: 5% de desconto;
+    //      - Outras categorias: Nenhum desconto;
+    // 4- Se a categoria for desconhecida ou não estiver na lista acima, o desconto será zero.
+    // 5- O valor do produto e a categoria serão sempre fornecidos como argumentos da função.
+
+    
+
+
