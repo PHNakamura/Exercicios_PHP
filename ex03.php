efe753924babc8b5117892ab4a30a59df0c02e15
PHNakamura <paulo_hn@hotmail.com>
2024-02-05 15:49:28 -0400

    Criacao do metodo

--- ex03.php
+++ ex03.php
@@ -12,6 +12,32 @@
     // 4- Se a categoria for desconhecida ou não estiver na lista acima, o desconto será zero.
     // 5- O valor do produto e a categoria serão sempre fornecidos como argumentos da função.
 
-    
+    /**
+     * Método para calcular desconto de um produto em função da sua categoria
+     * @param float|integer $v (valores)
+     * @param string $c (categoria)
+     * @return float|integer 
+     */
+    function calcularDesconto($v, $c){
+        $cat = [$c == 'Eletrônico'=>1.1, 'Vestuário'=>1.2, 'Alimentos'=>1.05];
+        $cat_el = $cat['Eletrônico'];
+        $cat_vest = $cat['Vestuário'];
+        $cat_al = $cat['Alimentos'];
+        $msg = "O novo valor será de R$: ";
+
+        if($c == "Eletrônico" || $c == "Eletronico" || $c == "eletrônico" || $c == "eletronico"){
+            $novo_vl = round(($v / $cat_el), 2);
+            echo $msg.$novo_vl . "<br>";
+        } else if ($c == "Vestuário" || $c == "Vestuario" || $c == "vestuário" || $c == "vestuario"){
+            $novo_vl = round(($v / $cat_vest),2);
+            echo $msg.$novo_vl . "<br>";
+        } else if($c == "Alimentos" || $c == "alimentos"){
+            $novo_vl = round(($v / $cat_al),2);
+            echo $msg.$novo_vl . "<br>";
+        } else {
+            echo "Categoria desconhecida. <br>";
+            echo "O valor não será alterado. <br>";
+        }
+    }
 
 
